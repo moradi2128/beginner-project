c3e24475246ed657a53064ecf93a7a3793e4d6df
mohammadreza moradi <moradi2565@gmail.com>
2022-03-05 18:53:04 +0330

    update readme.md

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,105 +1,111 @@
-import React, { useState, useEffect } from 'react'
-import { FaBars } from 'react-icons/fa';
-import { animateScroll as scroll } from 'react-scroll/modules';
+import React, { useState, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { animateScroll as scroll } from "react-scroll/modules";
 import {
-    Nav,
-    NavbarContainer,
-    NavLogo,
-    MobileIcon,
-    NavMenu,
-    NavItem,
-    NavLinks,
-    NavBtn,
-    NavBtnLink,
-} from './NavbarElement'
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElement";
 const Navbar = (props) => {
+  const [scrollNav, setScrollNav] = useState(false);
 
-    const [scrollNav, setScrollNav] = useState(false)
-
-
-    const changeNav = () => {
-        if (window.scrollY >= 80) {
-            setScrollNav(true)
-        } else {
-            setScrollNav(false)
-        }
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
     }
+  };
 
-    useEffect(() => {
-        window.addEventListener('scroll', changeNav)
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
 
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+  return (
+    <>
+      <Nav scrollNav={scrollNav}>
+        <NavbarContainer>
+          <NavLogo to="/" onClick={toggleHome}>
+            WebFarda
+          </NavLogo>
+          <MobileIcon onClick={props.toggleHandler}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks
+                to="about"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                درباره ما
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks
+                to="discover"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                پیشنهاد ویژه
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks
+                to="services"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                خدمات
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks
+                to="signup"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                ثبت نام
+              </NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink
+              to="/signin"
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact="true"
+              offset={-80}
+            >
+              ورود
+            </NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
 
-    const toggleHome = () => {
-        scroll.scrollToTop()
-    }
-    return (
-        <>
-            <Nav scrollNav={scrollNav}>
-                <NavbarContainer >
-                    <NavLogo to="/" onClick={toggleHome}>WebFarda</NavLogo>
-                    <MobileIcon onClick={props.toggleHandler}>
-                        <FaBars />
-                    </MobileIcon>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLinks
-                                to="about"
-                                smooth={true}
-                                duration={500}
-                                spy={true}
-                                exact='true'
-                                offset={-80}
-
-                            >درباره ما</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks
-                                to="discover"
-                                smooth={true}
-                                duration={500}
-                                spy={true}
-                                exact='true'
-                                offset={-80}
-
-                            >پیشنهاد ویژه</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks
-                                to="services"
-                                smooth={true}
-                                duration={500}
-                                spy={true}
-                                exact='true'
-                                offset={-80}
-                            >خدمات</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks
-                                to="signup"
-                                smooth={true}
-                                duration={500}
-                                spy={true}
-                                exact='true'
-                                offset={-80}
-                            >ثبت نام</NavLinks>
-                        </NavItem>
-
-                    </NavMenu>
-                    <NavBtn>
-                        <NavBtnLink
-                            to="/signin"
-                            smooth={true}
-                            duration={500}
-                            spy={true}
-                            exact='true'
-                            offset={-80}
-                        >ورود</NavBtnLink>
-                    </NavBtn>
-                </NavbarContainer>
-            </Nav>
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
